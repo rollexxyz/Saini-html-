a409fc6ac0bb5e92a9bed86b6c5b0ed1026e123e
rollexxyz <yadavjijmb@gmail.com>
2025-09-01 15:08:34 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,13 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "27813525"))
+API_HASH = environ.get("API_HASH", "4c422dcd8b5d472bfe99f6f66953b96c")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8394739429:AAEKRRUHBUHa8KO8oQfYPCxQPtrWNXjd1CM")
+CREDIT = environ.get("CREDIT", "𝐀𝐍𝐓𝐀𝐑𝐘𝐀𝐌𝐈 𝐁𝐎𝐓")
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
